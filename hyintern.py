9c0ef965ef92e31df3423767fe8d08fe5b128aa6
Paul Kim <kmc5500@naver.com>
2015-09-09 03:36:56 +0900

    add location

--- hyintern.py
+++ hyintern.py
@@ -1,12 +1,17 @@
 #-*- coding: utf-8 -*-
+# encoding=utf8
 #flaskr.py
 
 from __future__ import with_statement
 from contextlib import closing
 
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for , \
-	abort, render_template, flash
+	abort, render_template, flash, make_response
 from flask import json
 
 
@@ -21,6 +26,12 @@ LOCATION = {'seoul':'서울', 'guri':'구리', 'chang':'창원', 'jeju':'제주'
 # create our little app.
 app = Flask(__name__)
 app.config.from_object(__name__)
+
+# set location by cookie
+if(hasattr(request ,'cookie')):
+    location = request.cookie.get('location')
+else :
+    location = 'seoul'
 
 def connect_db():
 	return sqlite3.connect(app.config['DATABASE'])
@@ -39,24 +50,36 @@ def before_request():
 def teardown_request(exception):
 	g.db.close()
 
+
 @app.route('/')
-def show_phone_num_list():
-	cur = g.db.execute('select title, phone_num from phone_num_list order by id desc')
-	phone_num_list = [dict(title=row[0], phone_num=row[1]) for row in cur.fetchall()]
-	return render_template('show_phone_num_list.html', list=phone_num_list)
+def index_page():
+    return redirect(url_for('show_phone_num_list', location=location))
+
+@app.route('/show/<location>')
+def show_phone_num_list(location=location):
+    query = "select title, phone_num from phone_num_list where location='%s' order by id desc" % location
+    cur = g.db.execute(query)
+    
+    phone_num_list = [dict(title=row[0], phone_num=row[1]) for row in cur.fetchall()]
+    resp = make_response(render_template('show_phone_num_list.html', list=phone_num_list, location_str=LOCATION[location]))
+    resp.set_cookie('location', location)
+    print(location)
+    return resp
+
+
 
 @app.route('/edit', methods=['GET'])
 def edit_mode():
-    cur = g.db.execute('select id, title, phone_num from phone_num_list order by id desc')
-    phone_num_list = [dict(id=row[0], title=row[1], phone_num=row[2]) for row in cur.fetchall()]
+    cur = g.db.execute('select id, title, phone_num, location from phone_num_list order by id desc')
+    phone_num_list = [dict(id=row[0], title=row[1], phone_num=row[2], location=row[3]) for row in cur.fetchall()]
     return render_template('edit_mode.html', list=phone_num_list )
 
 @app.route('/add', methods=['POST'])
 def add_phone_num():
     if not session.get('logged_in'):
         abort(401)
-    g.db.execute('insert into phone_num_list (title, phone_num) values (?, ?)',
-                 [request.form['title'], request.form['phone_num']])
+    g.db.execute('insert into phone_num_list (title, phone_num, location) values (?, ?, ?)',
+                 [request.form['title'], request.form['phone_num'] , request.form['location']])
     g.db.commit()
     flash('New phone number was successfully posted')
     return redirect(url_for('show_phone_num_list'))
@@ -66,11 +89,13 @@ def delete(id=None):
     if not session.get('logged_in'):
         abort(401)
     query = "delete from phone_num_list where id= '%s'" % request.form['id'] 
-    print("query : ",query)
+    #print("query : ",query)
     g.db.execute(query)
     g.db.commit()
     flash(id+'has deleted')
     return redirect(url_for('edit_mode'), code='303')
+
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -95,4 +120,5 @@ def logout():
     return redirect(url_for('show_phone_num_list'))
 
 if __name__ == '__main__':
+	app.DEBUG = True;
 	app.run(port=80)
--- schema.sql
+++ schema.sql
@@ -3,5 +3,6 @@ create table phone_num_list (
 	id		integer primary key autoincrement,
 	title	string not null,
 	text	string not null default '',
+	location string not null default 'seoul',
 	phone_num	string not null
 );
