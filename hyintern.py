766edf446d99f86674cac0698adff001542c0abd
Paul Kim <kmc5500@naver.com>
2015-09-09 03:37:37 +0900

    add location

--- hyintern.py
+++ hyintern.py
@@ -120,5 +120,5 @@ def logout():
     return redirect(url_for('show_phone_num_list'))
 
 if __name__ == '__main__':
-	app.DEBUG = True;
-	app.run(port=80)
+	#app.DEBUG = True;
+	app.run(host='0.0.0.0', port=80)
